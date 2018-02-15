2a2d4a4c125bb3815373cdb6942b57c39ca5c2b1
Murilo <murilo.fantunes@gmail.com>
2018-02-15 17:19:33 -0200

    avancado v4

---
+++ javascript-avancado-iii-aula3/javascript-avancado-iii-aula3/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,116 @@
+class NegociacaoController {
+  constructor() {
+    let $ = document.querySelector.bind(document)
+
+    this._inputData = $('#data')
+    this._inputQuantidade = $('#quantidade')
+    this._inputValor = $('#valor')
+
+    this._listaNegociacoes = new Bind(
+      new ListaNegociacoes(),
+      new NegociacoesView($('#negociacoesView')),
+      'adiciona',
+      'esvazia',
+      'ordena',
+      'inverteOrdem'
+    )
+
+    this._mensagem = new Bind(
+      new Mensagem(),
+      new MensagemView($('#mensagemView')),
+      'texto'
+    )
+
+    this._ordemAtual = ''
+
+  this._init()    
+  }
+
+  _init () {
+    ConnectionFactory.getConnection()
+      .then(connection => new NegociacaoDao(connection))
+      .then(dao => dao.listaTodos())
+      .then(negociacoes =>
+        negociacoes.forEach(negociacao =>
+          this._listaNegociacoes.adiciona(negociacao)
+        )
+      )
+      .catch(erro => {
+        console.log(erro)
+        this._mensagem.texto = error
+      })
+    
+    setInterval(() => {
+      this.importaNegociacoes()
+    }, 3000)
+  }
+
+  adiciona(event) {
+    event.preventDefault()
+
+    ConnectionFactory.getConnection()
+      .then(conexao => {
+        let negociacao = this._criaNegociacao()
+
+        new NegociacaoDao(conexao).adiciona(negociacao).then(() => {
+          this._listaNegociacoes.adiciona(negociacao)
+          this._mensagem.texto = 'Negociação adicionada com sucesso'
+          this._limpaFormulario()
+        })
+      })
+      .catch(erro => (this._mensagem.texto = erro))
+  }
+
+  importaNegociacoes() {
+    let service = new NegociacaoService()
+    service
+      .obterNegociacoes()
+      .then(negociacoes =>
+        negociacoes.filter(negociacao =>
+          !this._listaNegociacoes.negociacoes.some(
+            negociacaoExistente =>
+              JSON.stringify(negociacao) == JSON.stringify(negociacaoExistente)
+          )
+        )
+      )
+      .then(negociacoes => negociacoes.forEach(negociacao => {
+          this._listaNegociacoes.adiciona(negociacao)
+          this._mensagem.texto = 'Negociações do período importadas'
+        }))
+      .catch(erro => (this._mensagem.texto = erro))
+  }
+
+  apaga() {
+    ConnectionFactory.getConnection()
+      .then(connection => new NegociacaoDao(connection))
+      .then(dao => dao.apagaTodos())
+      .then(mensagem => {
+        this._mensagem.texto = mensagem
+        this._listaNegociacoes.esvazia()
+      })
+  }
+
+  _criaNegociacao() {
+    return new Negociacao(
+      DateHelper.textoParaData(this._inputData.value),
+      parseInt(this._inputQuantidade.value),
+      parseFloat(this._inputValor.value)
+    )
+  }
+
+  _limpaFormulario() {
+    this._inputData.value = ''
+    this._inputQuantidade.value = 1
+    this._inputValor.value = 0.0
+    this._inputData.focus()
+  }
+
+  ordena(coluna) {
+    if (this._ordemAtual == coluna) {
+      this._listaNegociacoes.inverteOrdem()
+    } else {
+      this._listaNegociacoes.ordena((p, s) => p[coluna] - s[coluna])
+    }
+    this._ordemAtual = coluna
+  }
+}
